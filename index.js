b0ae605b5869cba9c34bc3eef0da8b87d6cad264
Subham <subham0506singh@gmail.com>
2025-04-23 18:18:13 +0530

    backend commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+const port =3000
+
+app.get('/',(req,res)=>{
+    res.send('hello world')
+})
+app.get('/twitter',(req,res)=>{
+    res.send('hello world')
+})
+
+app.listen(port,()=>{
+    console.log(`example app listening ${port}`)
+})
